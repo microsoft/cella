28fa1fac691576d557e71953a53181a9d7276323
Garrett Serack <garretts@microsoft.com>
2021-02-11 14:24:44 -0800

    clean up translator

--- translator/entrypoint.ts
+++ translator/entrypoint.ts
@@ -9,17 +9,20 @@ import { ForEachDescendantTraversalControl, Node, Project, PropertyAssignment, P
 import { parseArgs } from './lib/command-line';
 import { Dictionary } from './lib/linq';
 
+/** ensures a string is string encoded and quoted correctly  */
 function singleQuote(text: string) {
   return JSON.stringify(text).replace(/'/g, '\\\'').replace(/^"(.*)"$/, '\'$1\'');
 }
 
+/** removes backtick quotes from a string */
+function unquote(text: string) {
+  return (text.startsWith('`') && text.endsWith('`')) ? text = text.substr(1, text.length - 2) : text;
+}
 
-type parameters = { name: string, type: string }; //, tn: Type
-type translator = { literal: string; params: Array<parameters> };
-
+type templateStringParameter = { name: string, type: string };
+type templateStringInfo = { literal: string; params: Array<templateStringParameter> };
 
 const args = argv.slice(2);
-
 
 function version() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -51,32 +54,28 @@ async function main() {
     const root = commandline.folder;
     strict.ok(isDirectory(root), `${root} should be a project folder`);
     let updatedFiles = 0;
-    const key = commandline.switch('key', '--key specified more than once') || process.env['translator_key'] || fail('Missing translator key (--key or environment variable \'translator_key\') ';
+    const key = commandline.switch('key', '--key specified more than once') || process.env['translator_key'] || fail('Missing translator key (--key or environment variable \'translator_key\') ');
 
     header();
 
     const azure = new TranslatorTextClient(new CognitiveServicesCredentials(key), 'https://api.cognitive.microsofttranslator.com/');
-
-
-    const i18n = commandline.output || join(root, 'i18n');
+    const translationFilesFolder = commandline.output || join(root, 'i18n');
 
     const project = new Project({ tsConfigFilePath: join(root, 'tsconfig.json') });
-    const translationFiles = project.getSourceFiles().filter(each => normalize(each.getFilePath()).startsWith(i18n));
-    const sourceFiles = project.getSourceFiles().filter(each => !normalize(each.getFilePath()).startsWith(i18n));
+    const translationFiles = project.getSourceFiles().filter(each => normalize(each.getFilePath()).startsWith(translationFilesFolder));
+    const sourceFiles = project.getSourceFiles().filter(each => !normalize(each.getFilePath()).startsWith(translationFilesFolder));
 
     // right now it assumes the i18n function is in ${project}/lib/i18n.ts
     const i18nSourceFile = project.getSourceFiles().find(each => normalize(each.getFilePath()).startsWith(join(root, 'lib', 'i18n.ts')));
 
-    const strings: Dictionary<translator> = {};
+    const strings: Dictionary<templateStringInfo> = {};
+
     console.log(`${chalk.cyan('Project: ')}${root}\n${chalk.cyan('Source files to scan: ')}${sourceFiles.length}`);
 
     // track the i`` strings
     // eslint-disable-next-line no-inner-declarations
-    function track(key: string, literal: string, params: Array<parameters> = []) {
-      if (key.startsWith('`') && key.endsWith('`')) {
-        key = key.substr(1, key.length - 2);
-      }
-      strings[key] = { literal, params };
+    function track(key: string, literal: string, params: Array<templateStringParameter> = []) {
+      strings[unquote(key)] = { literal, params };
     }
 
     for (const sourceFile of sourceFiles) {
@@ -86,15 +85,17 @@ async function main() {
           if (Node.isTaggedTemplateExpression(node)) {
             const template = node.getTemplate();
             const tag = node.getTag();
-            // quick and dirty. just see if it's (....).i
             const tagType = tag.getType();
             const tagSymbol = tagType.getSymbol();
+
+            // having found our tagged template literal, let's make sure that the declaration matches
+            // the i`` one from our source file.
             const decl = tagSymbol?.getDeclarations()[0];
             if (Node.isFunctionDeclaration(decl)) {
               const sourceFile = decl?.getSourceFile();
-              if (decl.getName() === 'i' && sourceFile === i18nSourceFile)
+              if (decl.getName() === 'i' && sourceFile === i18nSourceFile) {
 
-                // we have found a tagged template literal.
+                // we have found a tagged template literal, and it's one of ours.
                 // let's start gathering the data for it.
                 if (Node.isNoSubstitutionTemplateLiteral(template)) {
                   // simple, no params
@@ -105,12 +106,15 @@ async function main() {
                   const templateHead = template.getHead();
                   let key = templateHead.getText();
                   let literal = templateHead.getText();
-                  const parameters = new Array<parameters>();
-                  let n = 0;
+
+                  // get the parameterized sections that follow
+                  const parameters = new Array<templateStringParameter>();
+                  let parameterNumber = 0;
                   for (const span of template.getTemplateSpans()) {
                     const content = span.getChildAtIndex(0);
                     const text = span.getChildAtIndex(1);
-                    key = `${key}${n}${text.getText()}`;
+                    // the key has to have ${number} instead of the template expression in it.
+                    key = `${key}${parameterNumber}${text.getText()}`;
 
                     if (Node.isIdentifier(content)) {
                       // it's a simple identifier,
@@ -124,37 +128,45 @@ async function main() {
                       parameters.push({ name, type });
                       literal = literal + name + text.getText();
                     } else if (Node.isExpression(content)) {
+                      // it's some kind of expression, we can get the type,
+                      // but the name will have to be generated ('p<number>')
                       // just use a placeholder number for now.
                       const type = node.getType().getText();
-                      const name = `p${n}`;
+                      const name = `p${parameterNumber}`;
 
                       parameters.push({ name, type });
                       literal = literal + name + text.getText();
                     }
-
+                    parameterNumber++;
                   }
+                  // now that we have all the parameters, lets track this one
                   track(key, literal, parameters);
-                  n++;
                 }
+              }
             }
           }
         } catch {
           //
         }
       });
-      // let's make sure there are entries in all of the language translations
     }
 
+    // let's make sure there are entries in all of the language translations
     for (const lang of translationFiles) {
+      // get the language name from the source file name
       const language = basename(lang.getFilePath()).replace(/\.ts$/, '');
 
-      let modified = false;
+      let isModified = false;
+
+      // find the declaration of 'map'
       const map = lang.getVariableDeclarations().find(variable => variable.getSymbol()?.getEscapedName() === 'map');
       if (Node.isVariableDeclaration(map)) {
         const initializer = map.getInitializer();
         const props: Dictionary<PropertyAssignment | null> = {};
 
         if (Node.isObjectLiteralExpression(initializer)) {
+
+          // record all the properties we have already.
           for (const prop of initializer.getProperties()) {
             if (Node.isPropertyAssignment(prop)) {
               const name = prop.getName();
@@ -163,13 +175,14 @@ async function main() {
             }
           }
 
-          // got all the properties.
           // let's see if all of our strings are in there.
           for (const key in strings) {
             const fn = strings[key];
             const name = singleQuote(key);
 
             if (props[name] === undefined) {
+              // missing a string! let's add it.
+
               const pp = fn.params.map(each => `${each.name}: ${each.type}`);
               const text = pp.length === 0 ? name : fn.literal;
               let translation = '';
@@ -177,23 +190,31 @@ async function main() {
                 const placeholders = new Array<string>();
                 let i = 0;
 
+                // whackety-whack, this is a hack.
+                // numbers seem to make it thru translation without
+                // too much trouble, so let's stick in a weird number
+                // for each parameter so we can put the parameter name
+                // back at the end.
                 const t = text.replace(/(\$\{.*?\})/g, (item) => {
                   placeholders[i] = item;
                   // insert a number that's not likely to be there
                   return `77${i++}77`;
                 });
 
+                // Azure, please translate this for me
                 const result = await azure.translator.translate([language], [{ text: t }]);
                 translation = result[0]?.translations?.[0].text || text;
+
+                // if we had whackety-whack parameters, let's fix them back up
                 if (i > 0) {
                   translation = translation.replace(/77(.*?)77/g, (item, value, pos, string) => {
                     // return the original template to the string
                     return placeholders[Number.parseInt(value)];
                   });
-                  // backtick quote it
+                  // backtick quote it (parameterized strings are templates in the translation.)
                   translation = '`' + translation.substr(1, translation.length - 2) + '`';
                 } else {
-                  // single quote it
+                  // single quote it (non-parameterized strings are just single quoted strings)
                   translation = '\'' + translation.substr(1, translation.length - 2) + '\'';
                 }
               } catch (e) {
@@ -203,8 +224,7 @@ async function main() {
               if (!translation) {
                 translation = text;
               }
-              // no property for that string. create it with the default impl
-
+              // Now we can create the property assignment for that string
               initializer.addProperty(<PropertyAssignmentStructure>{
                 name,
                 kind: StructureKind.PropertyAssignment,
@@ -213,11 +233,14 @@ async function main() {
               return ${translation};
             }`
               });
-              modified = true;
+              // keep track that we're modifying this file.
+              isModified = true;
             }
           }
         }
-        if (modified) {
+
+        // only save files that we actually touch.
+        if (isModified) {
           // clean up formatting
           lang.formatText({
             indentSize: 2,
