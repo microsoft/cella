35e9a4fab3b6104c179eddc5d1139e864161bb99
Garrett Serack <garretts@microsoft.com>
2021-02-19 07:54:27 -0800

    updated translation

--- cli/i18n/de.ts
+++ cli/i18n/de.ts
@@ -3,5 +3,9 @@ export const map: language = {
   'core version: ${0}': (Version: string) => {
     // autotranslated using Azure Translator via 'translate-strings' tool (`core version: ${Version}`)
     return `Kernversion: ${Version}`;
+  },
+  '(C) Copyright 2021 Microsoft Corporation': () => {
+    // autotranslated using Azure Translator via 'translate-strings' tool ('(C) Copyright 2021 Microsoft Corporation')
+    return '(C) Copyright 2021 Microsoft Corporation';
   }
 };
