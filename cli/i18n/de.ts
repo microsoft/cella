7bf6f583730515e61cecdc70e6ec8d1ecd281d20
Garrett Serack <garretts@microsoft.com>
2021-02-26 12:14:30 -0800

    correct typo

--- cli/i18n/de.ts
+++ cli/i18n/de.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright 2021 (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 interface language { [key: string]: (...args: Array<any>) => string; }
 export const map: language = {
--- cli/lib/styling.ts
+++ cli/lib/styling.ts
@@ -1,10 +1,10 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright 2021 (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { Session } from '@microsoft/cella.core';
