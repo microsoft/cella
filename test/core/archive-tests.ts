9836e8f0c132c9c4522a2ded207582fa81bc6710
Garrett Serack <garretts@microsoft.com>
2021-04-15 07:16:52 -0700

    remove unix workaround

--- test/core/archive-tests.ts
+++ test/core/archive-tests.ts
@@ -135,9 +135,7 @@ describe('ZipUnpacker', () => {
     const targetUri = local.tempFolderUri.join('example');
     await unpacker.unpack(zipUri, targetUri, {});
     strict.equal((await targetUri.readFile('a.txt')).toString(), 'The contents of a.txt.\n');
-    if (isWindows) {
-      strict.equal((await targetUri.stat('a.txt')).mtime, Date.parse('2021-03-23T16:31:14.000Z'));
-    }
+    strict.equal((await targetUri.stat('a.txt')).mtime, Date.parse('2021-03-23T16:31:14.000Z'));
     strict.equal((await targetUri.readFile('b.txt')).toString(), 'The contents of b.txt.\n');
     strict.equal((await targetUri.readFile('c.txt')).toString(), 'The contents of c.txt.\n');
     strict.equal((await targetUri.readFile('only-not-directory.txt')).toString(),
@@ -318,9 +316,7 @@ describe('ZipUnpacker', () => {
 
 async function checkExtractedTar(targetUri: Uri): Promise<void> {
   strict.equal((await targetUri.readFile('a.txt')).toString(), 'The contents of a.txt.\n');
-  if (isWindows) {
-    strict.equal((await targetUri.stat('a.txt')).mtime, Date.parse('2021-03-23T09:31:14.000Z'));
-  }
+  strict.equal((await targetUri.stat('a.txt')).mtime, Date.parse('2021-03-23T09:31:14.000Z'));
   strict.equal((await targetUri.readFile('b.txt')).toString(), 'The contents of b.txt.\n');
   strict.equal((await targetUri.readFile('executable.sh')).toString(), '#/bin/sh\necho "Hello world!"\n\n');
   if (!isWindows) {
