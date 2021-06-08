52636324d34194d6e7a51b0084ccea435c86b202
Billy Robert O'Neal III <bion@microsoft.com>
2021-06-08 14:47:19 -0700

    Separate out VsixLocalFilesystem.

---
+++ core/lib/vsix-local-filesystem.ts
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { LocalFileSystem } from './local-filesystem';
+import { Session } from './session';
+import { Uri } from './uri';
+
+export class VsixLocalFilesystem extends LocalFileSystem {
+  private readonly vsixBaseUri : Uri | undefined;
+
+  constructor(session: Session) {
+    super(session);
+    const programData = session.environment['ProgramData'];
+    if (programData) {
+      this.vsixBaseUri = this.file(programData).join('Microsoft/VisualStudio/Packages');
+    }
+  }
+
+  /**
+   * Creates a new URI from a string, e.g. `http://www.msft.com/some/path`,
+   * `file:///usr/home`, or `scheme:with/path`.
+   *
+   * @param value A string which represents an URI (see `URI#toString`).
+   */
+  parse(value: string, _strict?: boolean): Uri {
+    return Uri.parseFilterVsix(this, value, _strict, this.vsixBaseUri);
+  }
+}
